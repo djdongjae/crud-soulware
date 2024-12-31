eb21b1cde92ef5e60810b1fe76d5e75812d90e04
djdongjae <jk47wee@gmail.com>
2024-12-31 18:54:30 +0900

    init: 프로젝트 생성

---
+++ src/main/java/kr/soulware/crudsoulware/CrudSoulwareApplication.java
@@ -0,0 +1,13 @@
+package kr.soulware.crudsoulware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSoulwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSoulwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/soulware/crudsoulware/CrudSoulwareApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.soulware.crudsoulware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSoulwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
