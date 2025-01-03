46d4d581659abac7852d4cf9c90b8bffe62f1ce2
djdongjae <jk47wee@gmail.com>
2025-01-03 17:02:17 +0900

    [#9] refactor: 인증 관련 성공 코드 추가

--- src/main/java/kr/soulware/crudsoulware/exception/SuccessCode.java
+++ src/main/java/kr/soulware/crudsoulware/exception/SuccessCode.java
@@ -14,12 +14,14 @@ public enum SuccessCode {
      */
     GET_POST_SUCCESS(HttpStatus.OK, "성공적 게시글을 조회했습니다"),
     UPDATE_POST_SUCCESS(HttpStatus.OK, "성공적으로 게시글을 수정했습니다"),
+    SIGN_IN_USER_SUCCESS(HttpStatus.OK, "로그인에 성공했습니다."),
 
 
     /**
      * 201 CREATED
      */
     CREATE_POST_SUCCESS(HttpStatus.CREATED, "성공적으로 게시글을 생성했습니다"),
+    CREATE_USER_SUCCESS(HttpStatus.CREATED, "성공적으로 유저를 생성하였습니다"),
 
     /**
      * 204 NO_CONTENT
